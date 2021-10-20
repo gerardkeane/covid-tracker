4644b1b3dd59a11bfe12a9ad94b87a1d78afb895
Gerard Keane <g.keane@me.com>
2021-10-20 22:16:28 +0100

    Styled header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
-  <header class="text-center bg-blue-600 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+  <header class="flex flex-row items-center justify-center bg-blue-600 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <v-icon class="coronavirus-icon" name="md-coronavirus" />&nbsp;Covid-19 Tracker
+    </div>
   </header>
 </template>
 
