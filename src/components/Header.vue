067d6de814c645cd99f6dabb22bd50d6805a287b
Gerard Keane <g.keane@me.com>
2021-10-20 10:35:17 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="text-center bg-blue-600 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
