df3beaffbd6308179af8161e1be67c94d7e76678
Gerard Keane <g.keane@me.com>
2021-10-20 22:15:23 +0100

    Add oh-vue-icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { MdCoronavirus, PrSpinner, PrSort } from "oh-vue-icons/icons";
+import "./assets/tailwind.css";
 
-createApp(App).use(router).mount('#app')
+addIcons(MdCoronavirus, PrSpinner, PrSort);
+
+const app = createApp(App);
+app.use(router);
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
