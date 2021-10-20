736baff7e906bb18e3fb7c9bde8fb96f4c81b3b4
Gerard Keane <g.keane@me.com>
2021-10-20 22:16:16 +0100

    Data title component

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-5xl font-bold">{{ text }}</h2>
+    <div class="text-xl mt-4 mb-10">{{ timeStamp }}</div>
+  </div>
+</template>
+<script>
+import moment from "moment";
+export default {
+  name: "DataTitle",
+  props: ["text", "dataDate"],
+  computed: {
+    timeStamp: function() {
+      return moment(this.dataDate).format("Do MMMM YYYY, h:mm:ss a");
+    },
+  },
+};
+</script>
+<style></style>
